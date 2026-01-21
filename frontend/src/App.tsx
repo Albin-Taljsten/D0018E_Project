43d0cd62da56bfe21e732a5d9213db0e6b1c5034
Albin Täljsten <48694925+Albintaljsten@users.noreply.github.com>
2026-01-21 15:53:09 +0100

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<h1>Home Page</h1>} />
+                <Route path="/about" element={<h1>About Page</h1>} />
+            </Routes>
+        </>
+    )
+}
+
+export default App
